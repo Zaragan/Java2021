25c267385eae83c04050aaca26b0428aa2efe9ff
Erik <erikpvc@gmail.com>
2021-05-20 11:02:56 +0200

    Update LoginServlet.java

--- ProyectoUsuarios/src/main/java/com/proyecto/presentacion/LoginServlet.java
+++ ProyectoUsuarios/src/main/java/com/proyecto/presentacion/LoginServlet.java
@@ -12,9 +12,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class LoginServlet
- */
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
