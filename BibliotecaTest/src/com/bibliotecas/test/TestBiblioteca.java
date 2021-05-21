95aa94f43865cacdc9922116887db91ae7ff796d
Erik <erikpvc@gmail.com>
2021-05-21 13:24:44 +0200

    Ejemplo de como usar biblioteca
    para que no se me olvide

---
+++ BibliotecaTest/src/com/bibliotecas/test/TestBiblioteca.java
@@ -0,0 +1,20 @@
+package com.bibliotecas.test;
+//	Click derecho en el proyecto
+//		Build Path
+//			Configure build Path
+//			Pestaña Proyectos
+//			Añadir en Module Path la carpeta del proyecto Bibioteca
+//			Importar estaticamente el archivo java
+import static consola.Consola.*;
+
+import consola.Consola;
+public class TestBiblioteca {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		consola.Consola.p("test");
+		Consola.p("test1");
+		p("test2");
+	}
+
+}
---
+++ BibliotecaTest/src/module-info.java
@@ -0,0 +1,3 @@
+module bibliotecaTest {
+	requires Bibliotecas;
+}
