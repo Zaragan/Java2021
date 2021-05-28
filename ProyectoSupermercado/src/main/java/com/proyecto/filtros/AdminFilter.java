131f2b06e0e77afb8b00bc65f16571b19a14ce85
Erik <erikpvc@gmail.com>
2021-05-28 13:09:59 +0200

    Filtrado de admins

--- ProyectoSupermercado/src/main/java/com/proyecto/filtros/AdminFilter.java
+++ ProyectoSupermercado/src/main/java/com/proyecto/filtros/AdminFilter.java
@@ -9,43 +9,37 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet Filter implementation class AdminFilter
- */
-@WebFilter("/AdminFilter")
+import com.proyecto.entidades.Usuario;
+
+@WebFilter("/admin")
 public class AdminFilter implements Filter {
-
-    /**
-     * Default constructor. 
-     */
-    public AdminFilter() {
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see Filter#destroy()
-	 */
-	public void destroy() {
-		// TODO Auto-generated method stub
-	}
-
-	/**
-	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
-	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		// place your code here
+		HttpServletRequest req = (HttpServletRequest)request;
+		HttpServletResponse res = (HttpServletResponse)response;
+		
+		HttpSession session = req.getSession();
+		if(session.getAttribute("usuario") == null) {
+			req.setAttribute("error", "El usuario no tiene permiso para acceder");
+			req.getRequestDispatcher("/identificar.jsp").forward(req, res);
+		} else {
+			Usuario usuario = (Usuario) session.getAttribute("usuario");
+			if(usuario != null && usuario.getRol().equals(2)) {
+				chain.doFilter(request, response);
+			} else {
+				req.setAttribute("error", "El usuario no tiene permiso para acceder");
+				//res.sendRedirect(req.getContextPath() + "/login");
+				req.getRequestDispatcher("/identificar.jsp").forward(req, res);
+			}			
+		}
 
-		// pass the request along the filter chain
-		chain.doFilter(request, response);
+		//chain.doFilter(request, response);
 	}
 
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
-	public void init(FilterConfig fConfig) throws ServletException {
-		// TODO Auto-generated method stub
-	}
+	public void init(FilterConfig fConfig) throws ServletException {}
+	public void destroy() {}
 
 }
